2c14e88f52a6f540b78621337c3661b2e084cfe9
Ging123 <iceglauber@outlook.com>
2026-05-26 09:44:02 -0300

    Add database connection setup and integrate with FastAPI application

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+
 from src.presentation.routes.clients import router as clients_router
 from src.presentation.routes.webhooks import router as webhooks_router
+
+from src.infra.database.database import Base
+from src.infra.database.database import engine
 
 app = FastAPI()
 
 app.include_router(clients_router)
 app.include_router(webhooks_router)
+
+Base.metadata.create_all(bind=engine)
---
+++ src/infra/database/database.py
@@ -0,0 +1,30 @@
+from os import getenv
+from sqlalchemy import create_engine
+
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
