e1cf66831c0651198d85c58e1c8306ab2118469b
Ging123 <iceglauber@outlook.com>
2026-05-26 12:17:23 -0300

    Add ClientRepository class for managing client data operations, including creation and email existence check

---
+++ src/infra/repositories/client_repo.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+
+from src.infra.models.client_model import ClientModel
+from sqlalchemy import exists
+
+class ClientRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create(self, client: ClientModel) -> ClientModel:
+        self.db.add(client)
+        self.db.commit()
+        self.db.refresh(client)
+        return client
+
+    def email_already_exists(self, email: str) -> bool:
+      return self.db.query(
+        exists().where(
+            ClientModel.cliente_email == email
+        )
+      ).scalar()
