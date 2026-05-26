680abbe8b1d8bdfd1946930bb24ceb5e3d853a00
Ging123 <iceglauber@outlook.com>
2026-05-26 11:21:27 -0300

    Add ClientStatus and ClientPriority enums for client management

---
+++ src/domain/enums/client_enum.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class ClientStatus(str, Enum):
+    WAITING_ANALYSIS = "Aguardando Análise"
+    PROCESSED = "Processado"
+
+class ClientPriority(str, Enum):
+    HIGH = "prioridade_alta"
+    NORMAL = "prioridade_normal"
