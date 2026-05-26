0f2ebeaa1a2384f4bb00d5fb85eca8aa4ab6c8af
Ging123 <iceglauber@outlook.com>
2026-05-26 15:48:59 -0300

    Add UnitOfWork class for managing database transactions with Client and ProcessedEvents repositories

---
+++ src/infra/database/uow.py
@@ -0,0 +1,19 @@
+from src.infra.repositories.client_repo import ClientRepository
+from src.infra.repositories.processed_events_repo import ProcessedEventsRepository
+
+from sqlalchemy.orm import Session
+
+class UnitOfWork:
+    def __init__(self, db: Session):
+        self.session = db
+        self.clients = ClientRepository(db)
+        self.processed_events = ProcessedEventsRepository(db)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc, tb):
+        if exc:
+            self.session.rollback()
+        else:
+            self.session.commit()
