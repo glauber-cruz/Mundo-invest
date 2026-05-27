22b86765382c6f4f04127475809d21e67f072980
Ging123 <iceglauber@outlook.com>
2026-05-27 15:05:39 -0300

    Refactor main.py and conftest.py to enhance app initialization and testing setup
    - Introduced a create_app function in main.py to encapsulate FastAPI app creation and router inclusion.
    - Updated conftest.py to utilize the new create_app function for TestClient instantiation, ensuring a consistent testing environment.

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from fastapi import FastAPI
 from src.presentation.routes.clients import router as clients_router
 from src.presentation.routes.webhooks import router as webhooks_router
 
-app = FastAPI()
+def create_app() -> FastAPI:
+  app = FastAPI()
 
-app.include_router(clients_router)
-app.include_router(webhooks_router)
+  app.include_router(clients_router)
+  app.include_router(webhooks_router)
+
+  return app
+
+app = create_app()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,45 +1,45 @@
 import pytest
+from fastapi.testclient import TestClient
+
+from main import create_app
+from src.infra.database.database import get_db
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from sqlalchemy.pool import StaticPool
-
-from fastapi.testclient import TestClient
-
-from main import app
-
-from src.infra.database.database import get_db
 from src.infra.database.database import Base
 
-from src.infra.models.client_model import ClientModel
-from src.infra.models.processed_events_model import ProcessedEventModel
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    poolclass=StaticPool
+  SQLALCHEMY_DATABASE_URL,
+  connect_args={"check_same_thread": False},
+  poolclass=StaticPool
 )
 
 TestingSessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
+  autocommit=False,
+  autoflush=False,
+  bind=engine
 )
 
 Base.metadata.create_all(bind=engine)
 
+
 def override_get_db():
-    db = TestingSessionLocal()
+  db = TestingSessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
 
-    try:
-        yield db
-    finally:
-        db.close()
-
-app.dependency_overrides[get_db] = override_get_db
 
 @pytest.fixture
 def client():
-    return TestClient(app)
+  app = create_app()
+
+  app.dependency_overrides[get_db] = override_get_db
+
+  return TestClient(app)
